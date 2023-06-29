11861ec90344413417b66ea567b86955453fc7a9
ghproxy2 <138092713+ghproxy2@users.noreply.github.com>
2023-06-29 13:40:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-app.use('/', createProxyMiddleware((req) => {
-  const target = req.url.slice(1);
-  return { target, changeOrigin: true };
-}));
+app.use('/', (req, res, next) => {
+  const target = req.query.proxy;
+  if (target) {
+    createProxyMiddleware({ target, changeOrigin: true })(req, res, next);
+  } else {
+    next();
+  }
+});
 
 app.listen(3001);
